9797a0fce0bed9c17d8f9404bafae07e8cfc42e1
vincent <jonanv@uw.edu>
2014-11-05 17:11:49 -0800

    style class active

--- js/app.js
+++ js/app.js
@@ -5,3 +5,23 @@
     Add code here to create a new Angular application and controller.
     The array of employee objects is already in a global variable named 'pawneeEmployees'
 */
+
+
+"use strict";
+
+angular.module('AddressBook', [])
+    .controller('AddressController', function($scope) {
+        $scope.employees = pawneeEmployees;
+        $scope.sortCol = 'lastName';
+        $scope.searchString = '';
+        $scope.isActive = false;
+        $scope.sortBy = function(colName) {
+            if($scope.sortCol == colName) {
+                $scope.isActive = true;
+            }
+            else {
+                $scope.isActive = false;
+            }
+            $scope.sortCol = colName;
+        };
+    });
